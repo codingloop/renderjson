77a7e04b5420529ce9b0429c18a63bab15df0d37
Killer-I <ishunfs@gmail.com>
2020-11-06 17:48:34 +0530

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'renderjson'
+import 'renderjson/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
